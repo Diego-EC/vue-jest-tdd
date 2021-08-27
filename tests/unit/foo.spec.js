b6f119c0318859ecf23475365578c26dd57d4fe2
Diego <diegoezquerro@gmail.com>
2021-08-27 08:45:13 +0200

    first test added

---
+++ tests/unit/foo.spec.js
@@ -0,0 +1,26 @@
+import { shallowMount } from "@vue/test-utils";
+import foo from "@/components/foo.vue";
+
+describe("foo.vue", () => {
+
+  it('is vue instance', () => {
+    const wrapper = shallowMount(foo, {
+      propsData: {msg: 'bar'}
+    });
+    expect(wrapper.vm).toBeTruthy();
+  })
+
+  it("renders props.msg when passed", () => {
+    expect(true).toBe(true);
+  });
+
+  it("click", () => {
+    const wrapper = shallowMount(foo, {
+      propsData: {msg: 'bar'}
+    });
+    const onClick = jest.fn();
+    wrapper.setMethods({lol: onClick});
+    wrapper.find('button').trigger('click');
+    expect(onClick).toHaveBeenCalled();
+  });
+});
