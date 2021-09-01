026ea9b3e740ac8d5680a8167581b5edca847348
Diego Ezquerro Calvo <diegoezquerro@gmail.com>
2021-09-01 16:51:51 +0200

    added item and items layout and styles

--- src/app/shop/infrastructure/components/Item.vue
+++ src/app/shop/infrastructure/components/Item.vue
@@ -1,36 +1,91 @@
 <template>
-	<div>
-		<div>
-			<b-card
-			:title="'Item: ' + name"
-			img-src="https://picsum.photos/600/300/?image=25"
-			img-alt="Image"
-			img-top
-			tag="article"
-			style="max-width: 20rem"
-			class="mb-2"
-			>
-			<b-card-text>
-				Some quick example text to build on the card title and make up the
-				bulk of the card's content.
-			</b-card-text>
-
-			<b-button href="#" variant="primary">Go somewhere</b-button>
-			</b-card>
+	<div class="">
+		<b-card class="my-2 item__card dynamic-size" img-src="https://placekitten.com/480/480" img-alt="Image" img-top>
+            <b-row cols="2">
+                <b-col class="item__card-col" cols="10">
+                    <p class="text-left item__card-price">{{ price }} €</p>
+                </b-col>
+                <b-col class="item__card-col" cols="2">
+                    <b-button class="item__button" size="xs" href="#" variant="outline-light" >
+                        <b-icon icon="BIconHeart" variant="secondary"></b-icon>
+                    </b-button>
+                </b-col>
+            </b-row>
+            <b-card-text class="item__card-name mt-2">
+                {{ name }}
+            </b-card-text>
+		</b-card>
    	</div>
-  	</div>
 </template>
 
 <script>
+
 export default {
-	name: "Item",
+    name: "Item",
 	props: {
-   	name: String,
+        name: String,
 		description: String,
 		price: Number,
 	},
+    components: {
+
+    },
 };
 </script>
 
 <style lang="scss" scoped>
+    .item {
+        &__card {
+            margin-left: 1rem;
+            &-name {
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
+        }
+        &__card-col {
+            padding: 0;
+        }
+        &__card-price {
+            max-lines: 1;
+            word-wrap: normal;
+            font-weight: bold;
+            margin: 0 0 0 0.3rem;
+        }
+        &__button {
+            padding: 0;
+            background-color: alpha($color: #000000);
+            border-color: white;
+            &:active {
+                background-color: alpha($color: #000000) !important;
+                border-color: white !important;
+            }
+        }
+    }
+
+    @media only screen and (max-width: 600px) {
+        .dynamic-size{
+            height:16rem;
+        } 
+    }
+    @media only screen and (min-width: 600px) {
+        .dynamic-size{
+            height:22rem;
+        } 
+    } 
+    @media only screen and (min-width: 768px) {
+        .dynamic-size{
+            height:22rem;
+        } 
+    } 
+    @media only screen and (min-width: 992px) {
+        .dynamic-size{
+            height:22rem;
+        } 
+    } 
+    @media only screen and (min-width: 1200px) {
+        .dynamic-size{
+            height:24rem;
+        } 
+    }
 </style>
--- src/app/shop/infrastructure/components/Items.vue
+++ src/app/shop/infrastructure/components/Items.vue
@@ -1,8 +1,18 @@
 <template>
 	<div>
-		<Item name="foo" description="mario" :price=1 />
-		<Item name="bar" description="luigi" :price=2 />
-		<Item name="lol" description="bross" :price=3 />
+        <b-container class="">
+            <b-row class="mr-1" cols="2" cols-sm="2" cols-md="3" cols-lg="4">
+                <Item name="foo bar" description="mario" :price=123456789 />
+                <Item name="bar foo" description="luigi" :price=2 />
+                <Item name="mario bross" description="bross" :price=3 />
+                <Item name="luigi bross, luigi bross, luigi bross, luigi bross" description="bross" :price=4 />
+                <Item name="foo lol" description="mario" :price=5 />
+                <Item name="bar pep" description="luigi" :price=6 />
+                <Item name="lol bar" description="bross" :price=7 />
+                <Item name="pep foo" description="bross" :price=8 />
+                <Item name="pep bar" description="Ffoo" :price=9 />
+            </b-row>
+        </b-container>        
 	</div>
 </template>
 
@@ -12,7 +22,6 @@ export default {
   	name: "Items",
   	components: {
     	Item,
-
   	},
 };
 </script>
