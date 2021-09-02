e145a5c34533ee4e7234e5bb693d136090ca4664
Diego Ezquerro Calvo <diegoezquerro@gmail.com>
2021-09-02 18:04:31 +0200

    added vuex to the items - item v-for

--- src/app/shop/infrastructure/components/Items.vue
+++ src/app/shop/infrastructure/components/Items.vue
@@ -2,18 +2,9 @@
 	<div>
         <b-container class="">
             <b-row class="mr-1" cols="2" cols-sm="2" cols-md="3" cols-lg="4">
-                <Item name="foo bar" description="mario" :price=123456789 />
-                <Card name="pep bar" description="Ffoo" :price=9 />
-                <Item name="bar foo" description="luigi" :price=2 />
-                <Item name="mario bross" description="bross" :price=3 />
-                <Item name="luigi bross, luigi bross, luigi bross, luigi bross" description="bross" :price=4 />
-                <Item name="foo lol" description="mario" :price=5 />
-                <Item name="bar pep" description="luigi" :price=6 />
-                <Item name="lol bar" description="bross" :price=7 />
-                <Item name="pep foo" description="bross" :price=8 />
-                <Item name="pep bar" description="Ffoo" :price=9 />
+                <div v-for="(item, index) in $store.getters.items" :key="index"><Item :name=item.name :description=item.description :price=item.price /></div>
             </b-row>
-        </b-container>        
+        </b-container>
 	</div>
 </template>
 
@@ -28,5 +19,10 @@ export default {
     	Item,
         Card,
   	},
+    data() {
+    return {
+        array: ["HTML", "CSS", "Javascript", "Terminal"],
+    };
+  },
 };
 </script>
